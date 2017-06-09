40ba6416a6ca647e9c355460fa6ac042ba673e0f
eryueweb <eryue0123@163.com>
2017-06-09 11:08:57 +0800

    del user

--- huahua/data/user/userSqlMap.js
+++
@@ -1,8 +0,0 @@
-
-var user = {
-	add: 'insert into users set ?',
-	delete: 'delete from users where name="Kitty"',
-	update: 'update users set name=?,sex=?,age=?,email=? where userId=?',
-	search: 'select * from users'
-};
-module.exports = user;
